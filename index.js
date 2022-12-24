df6394c1830574698a9da4cf3272c422bb1d6753
Mak <infinitycorpkz@gmail.com>
2022-12-24 11:33:26 +0600

    add simple db

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const fs = require('fs');
+const JSONdb = require('simple-json-db');
 
 const app = express();
 
-const data = JSON.parse(fs.readFileSync(`./db.json`)) //Получаю JSON файлик и сразу парсю его
+const db = new JSONdb('./db.json'); //Получаю JSON файлик
+
 
 const getDataByMonth = (req, res) => {
 
    let month = +req.params.month  //Устанавливаю req.param на месяц
 
-   const filtered = data.timeForCode.filter(item => item.month === month)  //Применяю методы массива на JSON файл, фильтрую по месяцу
+   const filtered = db.storage.timeForCode.filter(item => item.month === month)  //Применяю методы массива на JSON файл, фильтрую по месяцу
    dayfilter = filtered.map(item => item.day)  //Массив дней из отфильтрованного массива
    timefilter = filtered.map(item => item.time)  //Массив времени из отфильтрованного массива
    ratefilter = filtered.map(item => item.rate);  //Массив оценок из отфильтрованного массива
-
 
    res.json({
       status: 'success',
@@ -22,10 +23,13 @@ const getDataByMonth = (req, res) => {
    })
 }
 
-app.get('/check/:month', getDataByMonth)    //Вызов по маршруту, и указанному месяцу
+
+
+app.get('/get_info_by_month/:month', getDataByMonth)    //Вызов по маршруту, и указанному месяцу
 
 
 const port = 8000;
 app.listen(port, () => {
    console.log(`App running on port ${port}...`);
+
 })

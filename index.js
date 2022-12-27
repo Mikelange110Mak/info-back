af8d3b99f0f3fa2076dac7995cea716c11c91bcd
Mak <infinitycorpkz@gmail.com>
2022-12-27 13:08:40 +0600

    add result function, add ejs

--- index.js
+++ index.js
@@ -3,18 +3,27 @@ const fs = require('fs');
 const JSONdb = require('simple-json-db');
 
 const app = express();
+app.set('view engine', 'ejs')      //Подключаю шаблонизатор EJS
+app.set('views', './views')        //Путь к шаблонизатору
+app.use(express.static('public'))  //Статические файлы (для CSS)
 
 const db = new JSONdb('./db.json'); //Получаю JSON файлик
+
+//Функция где хранятся мои массивы:
+function arrays(month) {
+   const filtered = db.storage.timeForCode.filter(item => item.month === month) //Применяю методы массива на JSON файл, фильтрую по месяцу
+   dayfilter = filtered.map(item => item.day) //Массив дней из отфильтрованного массива
+   timefilter = filtered.map(item => item.time) //Массив времени из отфильтрованного массива
+   ratefilter = filtered.map(item => item.rate); //Массив оценок из отфильтрованного массива
+   averageValue = timefilter.reduce((a, b) => (a + b), 0) //Среднее значение времени за месяц
+}
 
 
 const getDataByMonth = (req, res) => {
 
    let month = +req.params.month  //Устанавливаю req.param на месяц
 
-   const filtered = db.storage.timeForCode.filter(item => item.month === month)  //Применяю методы массива на JSON файл, фильтрую по месяцу
-   dayfilter = filtered.map(item => item.day)  //Массив дней из отфильтрованного массива
-   timefilter = filtered.map(item => item.time)  //Массив времени из отфильтрованного массива
-   ratefilter = filtered.map(item => item.rate);  //Массив оценок из отфильтрованного массива
+   arrays(month)
 
    res.json({
       status: 'success',
@@ -23,10 +32,34 @@ const getDataByMonth = (req, res) => {
    })
 }
 
+//Функция вывода результата в хтмле:
+const getResult = (req, res) => {
 
+   month = +req.params.month
+
+   arrays(month)
+   res.render('result', {
+      title: 'This is a title',
+      message: 'This is a message',
+      daysItems: dayfilter,
+      timeItems: timefilter,
+      rateItems: ratefilter,
+      average: Math.round(averageValue / dayfilter.length)
+   })
+}
+
+const chooseMonth = (req, res) => {
+   res.render('main', {
+      title: 'Главная'
+   })
+
+   console.log(req.query.month);
+}
+
+app.get('/', chooseMonth)
+app.get('/result/:month', getResult) //Вызов маршрута для вывода результата
 
 app.get('/get_info_by_month/:month', getDataByMonth)    //Вызов по маршруту, и указанному месяцу
-
 
 const port = 8000;
 app.listen(port, () => {
